4d8dd19ed968db24756b4d047d44968b58a08f8e
kashafdawood <kashaf.dawood@gmail.com>
2024-09-27 23:07:38 +0500

    add user task api call

---
+++ src/API/TaskAPI/addUserTask.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export default async function getUserTask(data) {
+  try {
+    const response = await axios.post(
+      `${process.env.REACT_APP_API_URL}tasks/`,
+      data,
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response;
+  } catch (err) {
+    throw err;
+  }
+}
